b4ada5b3b1e4a3cd33d755527c41df826b6be040
eldek <es.delfante@gmail.com>
2026-04-10 12:04:36 -0300

    Se termino bigint new y is_decimal_string

--- ent.c
+++ ent.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 // -- Bigint struct --
 typedef struct {
@@ -13,7 +15,28 @@ typedef struct {
    Creates a new Bigint from a decimal string
    Returns the Bigint, or NULL if the input is invalid
 */
-Bigint *bigint_new(const char *text); // EDU
+Bigint *bigint_new(const char *text){
+    if (!is_decimal_string(text)){
+        // NotAString
+        return NULL;
+    }
+
+    Bigint *bigint = malloc(sizeof(Bigint));
+    if (!bigint) {
+        // OutOfMemory
+        return NULL;
+    }
+
+    bigint->digits = text;
+    bigint->len = strlen(text);
+    if (text[0] == '-'){
+        bigint->sign = -1; // Negative
+    }
+    else {
+        bigint->sign = 1; // Positive
+    }
+    return bigint;
+}
 
 /*
     Prints the Bigint
@@ -34,7 +57,21 @@ Bigint *bigint_add(const Bigint *a, const Bigint *b); // IAN
     Verifies if a string is a valid decimal integer representation
     Returns 1 if valid, 0 otherwise
 */
-int is_decimal_string(const char *text); // EDU
+int is_decimal_string(const char *text){
+    if (text == NULL || *text == '\0') {
+        return 0;
+    }
+
+    for (int i = 0; i<strlen(text); i++){
+        char val = text[i];
+        if (i == 0 && val == '-'){continue;} // If its negative
+
+        if (!(val >= '0' && val <= '9')){
+            return 0;
+        }
+    }
+    return 1;
+}
 
 /*
     Returns the max lenght between two Bigints
@@ -53,5 +90,14 @@ int main(int argc, char* argv[]){
     for (int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
     }
+
+    char* text = "-1232323";
+    Bigint* bigint = bigint_new(text);
+    if (bigint){
+        for (int i = 0; i<strlen(bigint->digits); i++){
+            printf("%c", bigint->digits[i]);
+        }
+    }
+
     return 0;
 }
